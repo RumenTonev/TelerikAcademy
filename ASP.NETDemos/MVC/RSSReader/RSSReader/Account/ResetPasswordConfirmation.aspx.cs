3716733c82c09618be429bb69ab10817a8e6b53c
RumenTonev <rumen.tonev@yahoo.com>
2014-11-02 16:04:33 +0200

    New projects added
    New projects added

--- ASP.NETDemos/MVC/RSSReader/RSSReader/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace RSSReader.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- ASP.NETDemos/MVC/RSSReader/RSSReader/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RSSReader.Global" Language="C#" %>
